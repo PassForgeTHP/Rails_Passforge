25607fcca54d3cc0b91580fb39215e12d8c51137
melody-ste <melody_stephan@icloud.com>
2025-10-09 11:06:37 +0200

    adding name to model user + validates email + seed with names for the users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable,
 	       jwt_revocation_strategy: JwtDenylist
+
+         
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: VALID_EMAIL_REGEX, message: "n'est pas un email valide" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_02_082304) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"

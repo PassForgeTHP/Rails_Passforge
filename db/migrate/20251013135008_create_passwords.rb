b8aeaf177226c36fbc56f3aff51b2b1a3ba05fce
Paarroo <code_source_0@protonmail.com>
2025-10-13 15:47:28 +0200

    Generate Password model scaffold

---
+++ db/migrate/20251013135008_create_passwords.rb
@@ -0,0 +1,14 @@
+class CreatePasswords < ActiveRecord::Migration[8.0]
+  def change
+    create_table :passwords do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.string :username
+      t.text :password_encrypted
+      t.string :domain
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end

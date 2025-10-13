f749ebe641f662b17fce41dbf11bbce2807fcb38
melody-ste <melody_stephan@icloud.com>
2025-10-13 15:59:09 +0200

    db reset

--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,22 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_10_184551) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "avatar"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

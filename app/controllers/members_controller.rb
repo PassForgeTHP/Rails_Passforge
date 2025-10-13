6a6be5639d0a7ee6d05cbf808e198a4b6efb8d7b
melody-ste <melody_stephan@icloud.com>
2025-10-13 10:49:43 +0200

    def update in registrations controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,7 +5,11 @@ class MembersController < ApplicationController
     user = get_user_from_token
     render json: {
       message: "If you see this, you're in!",
-      user: user
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email
+      }
     }
   end
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -28,4 +28,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
+
+  def update
+    user = current_user
+
+    if user.update(account_update_params)
+      render json: {
+        message: 'Profile updated successfully.',
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email
+        }
+      }, status: :ok
+    else
+      render json: {
+        message: 'Failed to update profile.',
+        errors: user.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
 end

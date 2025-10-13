8f187b6e9fbe21a4e448bdca663f3f3b4512a986
Paarroo <code_source_0@protonmail.com>
2025-10-13 15:50:41 +0200

    Add NOT NULL constraints to passwords migration

--- db/migrate/20251013135008_create_passwords.rb
+++ db/migrate/20251013135008_create_passwords.rb
@@ -2,9 +2,9 @@ class CreatePasswords < ActiveRecord::Migration[8.0]
   def change
     create_table :passwords do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :title
+      t.string :title, null: false
       t.string :username
-      t.text :password_encrypted
+      t.text :password_encrypted, null: false
       t.string :domain
       t.text :notes
 

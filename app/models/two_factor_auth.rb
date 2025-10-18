ab79b17e7dcaf64736c89f6c6f3abea06b43f9e6
Paarroo <code_source_0@protonmail.com>
2025-10-19 00:01:39 +0200

    Create TwoFactorAuth model with encryption and backup code methods

---
+++ app/models/two_factor_auth.rb
@@ -0,0 +1,43 @@
+class TwoFactorAuth < ApplicationRecord
+  # Associations
+  belongs_to :user
+
+  # Validations
+  validates :user_id, presence: true, uniqueness: true
+  validates :secret_encrypted, presence: true
+  validates :enabled, inclusion: { in: [true, false] }
+
+  # Encrypt sensitive data using Rails built-in encryption
+  encrypts :secret_encrypted
+  encrypts :backup_codes_encrypted
+
+  # Check if a backup code is valid
+  # @param code [String] the backup code to verify
+  # @return [Boolean] true if the code matches one of the stored backup codes
+  def valid_backup_code?(code)
+    return false if backup_codes_encrypted.blank?
+
+    codes = JSON.parse(backup_codes_encrypted)
+    codes.any? { |hashed| BCrypt::Password.new(hashed) == code }
+  rescue JSON::ParserError, BCrypt::Errors::InvalidHash
+    false
+  end
+
+  # Mark a backup code as used by removing it from the list
+  # @param code [String] the backup code to invalidate
+  # @return [Boolean] true if the code was found and removed
+  def use_backup_code!(code)
+    return false if backup_codes_encrypted.blank?
+
+    codes = JSON.parse(backup_codes_encrypted)
+    index = codes.find_index { |hashed| BCrypt::Password.new(hashed) == code }
+
+    return false if index.nil?
+
+    codes.delete_at(index)
+    update(backup_codes_encrypted: codes.to_json)
+    true
+  rescue JSON::ParserError, BCrypt::Errors::InvalidHash
+    false
+  end
+end
